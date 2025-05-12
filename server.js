670bc409e6430a3c626aa1f1c9b9112259738355
Dipanshu Verma <vermadipanshu444@gmail.com>
2025-05-12 21:14:09 +0530

    remove cors

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const myBooksRoutes = require('./routes/mybooks');
 
 const app = express();
  
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(cookieParser());
  
